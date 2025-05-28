88961d13b0aa783304c429e4e48b2914919c0219
Daniel Esrey <dan.esrey@icloud.com>
2025-05-28 11:01:32 +0100

    initial

---
+++ EFLChampionship/EFLChampionshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  EFLChampionshipApp.swift
+//  EFLChampionship
+//
+//  Created by Daniel Esrey on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct EFLChampionshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EFLChampionshipTests/EFLChampionshipTests.swift
@@ -0,0 +1,17 @@
+//
+//  EFLChampionshipTests.swift
+//  EFLChampionshipTests
+//
+//  Created by Daniel Esrey on 27/05/2025.
+//
+
+import Testing
+@testable import EFLChampionship
+
+struct EFLChampionshipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
